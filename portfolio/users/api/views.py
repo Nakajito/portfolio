8d97fa8c80526e9ef4a7ffa4e9d8b9e5a2f1838f
Daniel <nakajito@proton.me>
2026-03-14 15:00:08 -0500

    chore: initial project setup with django ninja and uv

---
+++ portfolio/users/api/views.py
@@ -0,0 +1,46 @@
+from django.db.models import QuerySet
+from django.shortcuts import get_object_or_404
+from ninja import Router
+
+from portfolio.users.api.schema import UpdateUserSchema
+from portfolio.users.api.schema import UserSchema
+from portfolio.users.models import User
+
+router = Router(tags=["users"])
+
+
+def _get_users_queryset(request) -> QuerySet[User]:
+    return User.objects.filter(pk=request.user.pk)
+
+
+@router.get("/", response=list[UserSchema])
+def list_users(request):
+    return _get_users_queryset(request)
+
+
+@router.get("/me/", response=UserSchema)
+def retrieve_current_user(request):
+    return request.user
+
+
+@router.get("/{pk}/", response=UserSchema)
+def retrieve_user(request, pk: int):
+    users_qs = _get_users_queryset(request)
+    return get_object_or_404(users_qs, pk=pk)
+
+
+@router.patch("/me/", response=UserSchema)
+def update_current_user(request, data: UpdateUserSchema):
+    user = request.user
+    user.name = data.name
+    user.save()
+    return user
+
+
+@router.patch("/{pk}/", response=UserSchema)
+def update_user(request, pk: int, data: UpdateUserSchema):
+    users_qs = _get_users_queryset(request)
+    user = get_object_or_404(users_qs, pk=pk)
+    user.name = data.name
+    user.save()
+    return user
